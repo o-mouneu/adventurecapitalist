a1194642bfbea61733320f030e20ccfc6499f0c0
o-mouneu <oliviermouneu@outlook.fr>
2021-06-10 09:15:07 +0200

    Création serveur Spring (classes Java)

---
+++ serveurSpring/src/main/java/serveur/AdventureCapitalistApplication.java
@@ -0,0 +1,13 @@
+package serveur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdventureCapitalistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdventureCapitalistApplication.class, args);
+	}
+
+}
---
+++ serveurSpring/src/test/java/serveur/AdventureCapitalistApplicationTests.java
@@ -0,0 +1,13 @@
+package serveur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdventureCapitalistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
